5d21a0ee5c442b0f03bd64954ad2f26fb4de5214
Sav Younts <savyounts@gmail.com>
2018-08-03 17:41:09 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,14 @@
 def square_array(array)
-  new_array=[]
- array.each do |number|
-   number = number**2 
-   new_array << number
-  end 
-  return new_array
+  #new_array=[]
+# array.each do |number|
+   #number = number**2 
+   #new_array << number
+ # end 
+  #return new_array
+  array.collect do |number|
+    number**2 
+  end
+  
 end
+
 
