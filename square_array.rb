023498a6418b5cfb6a0fcd786f6a67cf1689895b
Sav Younts <savyounts@gmail.com>
2018-08-03 17:42:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,12 +3,10 @@ def square_array(array)
 array.each do |number|
    number = number**2 
    new_array << number
-
-  return new_array
-  #array.collect do |number|
+ #array.collect do |number|
     #number**2 
   end
-  
+   return new_array
 end
 
 
